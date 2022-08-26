bc7a46c2e898ffd044137f51948765f87c5e51fd
Yann Costa e Silva <ynncstslv@gmail.com>
2022-08-26 13:29:16 -0300

    Personal Portfolio V0

---
+++ script.js
@@ -0,0 +1,40 @@
+// navbar menu display
+
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+    document.body.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    if (window.innerWidth < 991) {
+        menu.classList.remove('fa-times');
+        header.classList.remove('active');
+        document.body.classList.remove('active');
+    }
+    // change active state
+    document.querySelectorAll('section').forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            document.querySelectorAll('.header .navbar a').forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.header .navbar a[href*='+ id +']').classList.add('active');
+            });
+        }
+    })
+}
+
+// scroll animations
+
+AOS.init({
+    duration: 500,
+    delay: 200,
+});
+
